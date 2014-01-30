62252f8122b280deb303d6013dc23861cac4f6e5
James Ndegwa Maringa <jmsmaringa@gmail.com>
2014-01-30 11:25:15 +0300

    Update newsscrape.py

--- newsscrape.py
+++ newsscrape.py
@@ -1,3 +1,4 @@
 '''
-a script to help parse news items ang tag them geographically
+A script to help parse news items ang tag them geographically
+I've been working on this for a while. I'll need to add a little more functionality before I can upload it
 '''
