77046e9b179f173211faa8ed0d4d0a9e8871721f
wandeg <jmsmaringa@gmail.com>
2016-03-25 12:46:11 +0300

    Addition of code that reads sql queries and writes the output of the analyze to a file

--- test_pg_explain.py
+++ test_pg_explain.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 import time
 import psycopg2
 import argparse
+import csv
 
 parser = argparse.ArgumentParser(description="Arguments to connect to the db")
 parser.add_argument('--verbose','-v',
@@ -21,3 +22,17 @@ database = args.db
 port = args.port
 user = args.user
 # password = args.password
+connection = psycopg2.connect(host = host, database = database, port = port, user = user)
+cur = connection.cursor()
+
+with open('tests.sql') as sqlfile:
+	with open('sql_output.csv','w') as outfile:
+		writer = csv.writer(outfile)
+		writer.writerow(['line','plan','time'])
+		for line in sqlfile:
+			cur.execute(line)
+			data = cur.fetchall()
+			plan = data[0]
+			time = data[1]
+			writer.writerow([line.strip(),str(plan).strip(),str(time).strip()])
+			
